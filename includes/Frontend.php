9fa072a0bb524008dbd5066da4a183a5961c77e0
anisur2805 <anisur2805@gmail.com>
2022-03-31 04:14:33 +0600

    resolve merge conflict

--- includes/Frontend.php
+++ includes/Frontend.php
@@ -5,21 +5,11 @@ use APC\Popup\Creator\Frontend\Modal;
 
 class Frontend {
       public function __construct() {
-            add_action('wp_enqueue_scripts', array( $this, 'load_assets' ) );
-            
-           
-            
-      }
+            wp_enqueue_style( 'puc-style' );
+            wp_enqueue_script( 'plain-modal' );
+            wp_enqueue_script( 'puc-main' );
+
+            new Modal();
+      } 
       
-      
-      /**
-       * Includes plugin asset files
-       */
-      public function load_assets() {
-      new Modal();
-            
-            wp_enqueue_style('popup-creator-style', PUC_PUBLIC_URL . '/css/popup-creator.css', time());
-            wp_enqueue_script('plain-modal', PUC_PUBLIC_URL . '/js/jquery.plainmodal.min.js', null, time(), true);
-            wp_enqueue_script('popup-creator-main', PUC_PUBLIC_URL . '/js/popup-main.js', array('jquery', 'plain-modal'), time(), true);
-      }
 }
