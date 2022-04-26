7cce702187cb9013310738a1a5d0309e36778ad7
anisur2805 <anisur2805@gmail.com>
2022-04-26 09:13:05 +0600

    implement react

--- includes/Frontend/Shortcode.php
+++ includes/Frontend/Shortcode.php
@@ -6,6 +6,8 @@ class Shortcode {
       public function __construct() {
             
             add_shortcode('arpc_newsletter', array($this, 'render_shortcode'));
+            add_shortcode('arpc_frontend', array($this, 'render_arpc_frontend'));
+
       }
 
      
@@ -18,6 +20,22 @@ class Shortcode {
 
             ob_start();
             include __DIR__ . "/views/signup-form.php";
+            return ob_get_clean();
+            
+            // $content = '<div>';
+            // $content .= '<p>Hello world</p>';
+            // $content .= '</div>';
+            // return $content;
+      }     
+
+      public function render_arpc_frontend($atts, $content) {
+            $atts = shortcode_atts(array(
+                  'title'           => __('Sign up for Snappy News!', 'popup-creator'),
+                  'content'         => __('Get Free WordPress Videos, Plugins, and Other Useful Resources', 'popup-creator'),
+            ), $atts, 'newsletter');
+
+            ob_start();
+            include __DIR__ . "/views/react-frontend.php";
             return ob_get_clean();
             
             // $content = '<div>';
