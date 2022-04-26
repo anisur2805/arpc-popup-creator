3e8a2fac2d9b68ccb3136fee48e33ca06e512014
anisur2805 <anisur2805@gmail.com>
2022-04-26 09:13:05 +0600

    implement react

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('arpc-popup-creator-wrapper'));
+const root = ReactDOM.createRoot(document.getElementById('react-frontend'));
 root.render(
   <React.StrictMode>
     <App />
