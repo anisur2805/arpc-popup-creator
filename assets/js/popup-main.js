03359ad1e58ab3f7516c50d874571b80088c6ce4
anisur2805 <anisur2805@gmail.com>
2022-09-15 14:47:18 +0600

    useCookie

--- assets/js/popup-main.js
+++ assets/js/popup-main.js
@@ -7,20 +7,23 @@
 		PlainModal.closeByEscKey = false;
 		PlainModal.closeByOverlay = false;
 		var modalsElms = document.querySelectorAll(".arpc-popup-creator");
-		var getShowOnce = JSON.parse(localStorage.getItem("showOnce"));
+		var getShowOnceRaw = localStorage.getItem("showOnce");
+		var getShowOnce = JSON.parse(getShowOnceRaw);
+
+		var siteName = window.location.hostname;
+		var currentPath = siteName.hostname;
 
 		for (var i = 0; i < modalsElms.length; i++) {
 			var modalContent = modalsElms[i];
 			var modal = new PlainModal(modalContent);
 			modal.closeButton = modalContent.querySelector(".arpc-close-button");
 			var displayPage = modalsElms[i].getAttribute("data-page");
-			console.log( displayPage )
-			
+
 			var delay = modalsElms[i].getAttribute("data-delay");
 			var isExit = modalsElms[i].getAttribute("data-exit");
 			if (isExit == 1) {
 				if (delay > 0) {
-					delayPopups.push({ modal: modal, delay: delay });
+					delayPopups.push({ modal: modal, delay: delay, displayPage: displayPage });
 				} else {
 					modal.open();
 					modal.overlayBlur = 3;
@@ -31,7 +34,6 @@
 		}
 
 		for (i in delayPopups) {
-			console.log("getShowOnce ", getShowOnce);
 			if (!getShowOnce) {
 				setTimeout(
 					function (i) {
@@ -40,29 +42,33 @@
 					delayPopups[i].delay,
 					i
 				);
-				
+
 			}
-
-			modal.closeButton.addEventListener('click', function () {
-				localStorage.setItem('showOnce', true);
-			});
-
 		}
 
-		// if (exitModals.length > 0) {
-		// 	window.onbeforeunload = function (event) {
-		// 		if (!popupDisplayed) {
-		// 			for (var i in exitModals) {
-		// 				exitModals[i].open();
-		// 			}
-		// 			popupDisplayed = true;
-		// 		}
-		// 		event.preventDefault();
-		// 		return event.returnValue = "random";
+		modal.closeButton.addEventListener('click', function () {
+			localStorage.setItem('showOnce', true);
+			arpcCookie(displayPage + "-ck", '1', 60);
+		});
 
-		// 	};
-		// }
+		function arpcCookie(cName, cVal, cExpiryInSeconds) {
+			var expiry = new Date();
+			expiry.setTime(expiry.getTime() + 1000 * cExpiryInSeconds);
+			document.cookie = cName + "=" + escape(cVal) + ";expires=" + expiry.toGMTString() + "; path=" + currentPath;
+		}
 
+		if (exitModals.length > 0) {
+			window.onbeforeunload = function (event) {
+				if (!popupDisplayed) {
+					for (var i in exitModals) {
+						exitModals[i].open();
+					}
+					popupDisplayed = true;
+				}
+				event.preventDefault();
+				return event.returnValue = "random";
 
+			};
+		}
 	});
 })(jQuery);
